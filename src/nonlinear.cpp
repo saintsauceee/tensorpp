a0dab91520048f6e9fa6fc52a3800f619ace0f6d
saintsauceee <t.donghao.zeng@gmail.com>
2025-12-06 04:29:41 -0500

    add secant method

--- src/nonlinear.cpp
+++ src/nonlinear.cpp
@@ -43,6 +43,49 @@ double newton(
     return x;
 }
 
+double secant(
+    const std::function<double(double)> &f,
+    double x0,
+    double x1,
+    double x_tol,
+    double f_tol,
+    int n_max,
+    bool verbose = false)
+{
+    double f0 = f(x0);
+    double f1 = f(x1);
+
+    for (int n = 1; n <= n_max; n++)
+    {
+        if (f1 - f0 == 0.0)
+        {
+            break; // avoid division by 0
+        }
+
+        double d = (x1 - x0) * f1 / (f1 - f0);
+
+        x0 = x1;
+        f0 = f1;
+        x1 = x1 - d;
+        f1 = f(x1);
+
+        if (verbose)
+        {
+            std::cout << "iter " << n
+                      << "  x = " << x1
+                      << "  f(x) = " << f1
+                      << "  step = " << d << '\n';
+        }
+
+        if (std::abs(d) <= x_tol or std::abs(f1) <= f_tol)
+        {
+            return x1;
+        }
+    }
+
+    return x1;
+}
+
 int main()
 {
     auto f = [](double x)
